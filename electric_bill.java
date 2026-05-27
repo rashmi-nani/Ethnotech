36fa03dc3d5f3bc9769635be3013f5b1fa9dc7e3
Pamuganipalli Rashmi <rashmipamuganipalli@gmail.com>
2026-05-27 10:48:59 +0530

    Add files via upload

---
+++ electric_bill.java
@@ -0,0 +1,21 @@
+package util;
+import java.util.*;
+public class Electric_bill {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int units=sc.nextInt();
+		int bill=0;
+		if(units>250)
+		{
+			int extra=(units-250);
+			int blocks=(extra+49)/50;
+			bill=(blocks<<5)+(blocks<<4)+(blocks<<1);
+			
+		}
+		System.out.println(bill);
+
+	}
+
+}
